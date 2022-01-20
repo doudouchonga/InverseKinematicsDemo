9502abf50274258c27390bce9cd4c54388eb65f9
xmcjwang <664973795@qq.com>
2022-01-20 17:01:41 +0800

    &&&

---
+++ Source/SplineDemo1/PaticleCubeActor.cpp
@@ -0,0 +1,141 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PaticleCubeActor.h"
+#include "Engine/StaticMeshActor.h"
+
+#include "Materials/Material.h"
+#include "../Plugins/Runtime/ProceduralMeshComponent/Source/ProceduralMeshComponent/Public/ProceduralMeshComponent.h"
+#include "../Plugins/Experimental/GeometryProcessing/Source/GeometricObjects/Public/MathUtil.h"
+
+// Sets default values
+APaticleCubeActor::APaticleCubeActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ProceduralMesh = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("mesh"));
+	MeshMaterial = LoadObject<UMaterial>(NULL, TEXT("/Game/StarterContent/Materials/M_Wood_Floor_Walnut_Worn"), NULL, LOAD_None, NULL);
+	
+}
+
+
+void APaticleCubeActor::Velet()
+{
+
+	for (int32 i = 0; i < Particles.Num(); i++)
+	{
+		if (!Particles[i].bFree)
+		{
+			continue;
+		}
+		// Find vel
+		const FVector Vel = Particles[i].CurPos - Particles[i].OldPos;
+		FVector TargetPosition = Particles[i].TargetPos - Particles[i].CurPos;
+		TargetPosition.Normalize();
+		// Update position
+		const FVector NewPosition = Particles[i].CurPos + Vel + (Particles[i].Speed * TargetPosition);
+		Particles[i].OldPos = Particles[i].CurPos;
+		Particles[i].CurPos = NewPosition;
+	}
+}
+
+void APaticleCubeActor::SolveDistance()
+{
+	for (FVConstrain& item : Constrains)
+	{
+		SolveDistance(item);
+	}
+
+}
+
+void APaticleCubeActor::SolveDistance(FVConstrain& Constrain)
+{
+	FVParticle* particleA = &Particles[Constrain.iparticleA];
+	FVParticle* particleB = &Particles[Constrain.iparticleB];
+
+	if (particleA == nullptr || particleB == nullptr) return;
+
+	float DisierDistance = 0;
+
+	FVector Delta = particleB->CurPos - particleA->CurPos;
+
+	float CurrentDistance = Delta.Size();
+	float ErrorFactor = (CurrentDistance - DisierDistance) / CurrentDistance;
+
+	if (particleA->bFree && particleB->bFree)
+	{
+		particleA->CurPos += ErrorFactor * 0.5f * Delta;
+		particleB->CurPos -= ErrorFactor * 0.5f * Delta;
+	}
+	else if (particleA->bFree)
+	{
+		particleA->CurPos += ErrorFactor * Delta;
+	}
+	else if (particleB->bFree)
+	{
+		particleB->CurPos -= ErrorFactor * Delta;
+	}
+}
+
+void APaticleCubeActor::CreateParticle(FVector pos, bool free)
+{
+	FVParticle item;
+	item.CurPos = pos;
+	item.OldPos = pos;
+	item.bFree = free;
+	Particles.Add(item);
+}
+
+void APaticleCubeActor::SetParticleTargetPos(int x, FVector pos)
+{
+	if (x < 0 || x >= Particles.Num())
+	{
+		return;
+	}
+	FVParticle& particleA = Particles[x];
+	particleA.SetTargetPos(pos);
+
+}
+
+void APaticleCubeActor::BuildConstrain(int x, int y)
+{
+	if (x < 0 || x >= Particles.Num())
+	{
+		return;
+	}
+	if (y < 0 || y >= Particles.Num())
+	{
+		return;
+	}
+	FVConstrain item;
+	item.iparticleA = x;
+	item.iparticleB = y;
+	Constrains.Add(item);
+
+	FVParticle* particleB = &Particles[y];
+	particleB->TargetPos = particleB->CurPos;
+	particleB->TargetPos.X = 0;
+
+}
+
+
+// Called when the game starts or when spawned
+void APaticleCubeActor::BeginPlay()
+{
+	Super::BeginPlay();
+		
+	/*PaticleCubeActor =  GetWorld()->SpawnActor<AUnitPaticleCubeActor>(AUnitPaticleCubeActor::StaticClass());
+
+	PaticleCubeActor->AttachToActor(this, FAttachmentTransformRules::KeepRelativeTransform);*/
+}
+
+// Called every frame
+void APaticleCubeActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	Velet();
+	SolveDistance();
+
+}
---
+++ Source/SplineDemo1/PaticleCubeActor.h
@@ -0,0 +1,104 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PaticleCubeActor.generated.h"
+
+USTRUCT(BlueprintType)
+struct FVParticle
+{
+	GENERATED_USTRUCT_BODY()
+	FVParticle() :
+		bFree(true),
+		OldPos(0, 0, 0),
+		CurPos(0, 0, 0)
+	{}
+
+	bool bFree;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector OldPos;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector CurPos;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector TargetPos;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Speed{0.1f};
+
+	void SetTargetPos(FVector pos)
+	{
+		TargetPos = pos;
+	}
+
+};
+
+USTRUCT(BlueprintType)
+struct FVConstrain
+{
+	GENERATED_USTRUCT_BODY()
+	FVConstrain() {}
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int iparticleA;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int iparticleB;
+	
+	void BuildConstrain(int A, int B)
+	{
+		iparticleA = A;
+		iparticleB = B;
+	}
+
+	
+	void DrawDebugConstrain(UWorld* world)
+	{
+		// DrawDebugCylinder(world, particleA->CurPos, particleB->CurPos, 1.0, 6, FColor::Blue, false, -1, 0, 1.0);
+	}
+
+};
+
+UCLASS(BlueprintType)
+class SPLINEDEMO1_API APaticleCubeActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APaticleCubeActor();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UProceduralMeshComponent* ProceduralMesh;
+
+	UPROPERTY()
+		class UMaterial* MeshMaterial;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FVParticle> Particles;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FVConstrain> Constrains;
+	
+	void Velet();
+	void SolveDistance();
+
+	UFUNCTION(BlueprintCallable)
+		void CreateParticle(FVector pos, bool free);
+
+	UFUNCTION(BlueprintCallable)
+		void SetParticleTargetPos(int x, FVector pos);
+
+	UFUNCTION(BlueprintCallable)
+	void BuildConstrain(int x, int y);
+
+	void SolveDistance(FVConstrain& Constrain);
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
